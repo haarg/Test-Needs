0ce19b77c3b4e82a577e5701615364834e7f2acb
Graham Knop <haarg@haarg.org>
2020-08-04 02:05:10 +0200

    support perl versions > 5

--- lib/Test/Needs.pm
+++ lib/Test/Needs.pm
@@ -61,7 +61,7 @@ sub _numify_version {
       : /^[0-9]+(?:\.[0-9]+)?$/ ? sprintf('%.6f', $_)
       : /^v?([0-9]+(?:\.[0-9]+)*)$/
         ? sprintf('%d.%03d%03d', ((split /\./, $1), 0, 0)[0..2])
-      : /^(\x05)(.*)$/s
+      : /^([\x05-\x07])(.*)$/s
         ? sprintf('%d.%03d%03d', ((map ord, "$1", (split //, "$2")), 0, 0)[0..2])
       : _croak qq{version "$_" does not look like a number};
   }
